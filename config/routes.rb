af03f88179cf32604de519bfcd8b7cc32a3ff3f5
ice48623 <ice48623@hotmail.com>
2017-03-20 18:18:31 +0700

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'application#hello'
+  get  '/signup',  to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
