a516e2e9f96ded97ffc1dbdd14acc9da0e3afbc1
ice48623 <ice48623@hotmail.com>
2017-03-27 00:24:27 +0700

    fix available team dropdown

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -53,8 +53,8 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(project_params)
-
+    @project = Project.new(:name => params[:project][:name], :description => params[:project][:description], :team_id => params[:team_id])
+    @project.save
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
@@ -102,8 +102,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      @team_id = params[:team_id]
-      params.require(:project).permit(:name, :description, @team_id)
+      params.require(:project).permit(:name, :description, :team_id)
     end
 
     def require_login
